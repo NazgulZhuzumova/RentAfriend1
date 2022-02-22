5b52276d632db0dcf0a607cdb7f8021a8c140d29
Jimmy Yuen <jimmehyuen@gmail.com>
2022-02-22 14:52:12 +0000

    validation and associations done

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,2 +1,10 @@
 class Friend < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :age, numericality: { only_integer: true }, presence: true
+  validates :location, presence: true
+  validates :gender, presence: true
+  validates :price, numericality: { only_integer: true }, presence: true
+  belongs_to :users
+  has_many :bookings, dependent: :destroy
+  has_many :users, through: :bookings
 end
