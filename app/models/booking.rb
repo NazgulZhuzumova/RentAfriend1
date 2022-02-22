5f26f28f3fa813f1463f54351687810824335b1e
Jimmy Yuen <jimmehyuen@gmail.com>
2022-02-22 16:34:10 +0000

    updated seeds

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,6 @@ class Booking < ApplicationRecord
   validates :location, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
-  belongs_to :users, :friends
+  belongs_to :users
+  belongs_to :friends
 end
