a53bbd8ed73f88ecb74014f558f3129a53a07079
Jimmy Yuen <jimmehyuen@gmail.com>
2022-02-23 12:33:52 +0000

    added cloudinary

--- Gemfile
+++ Gemfile
@@ -66,3 +66,4 @@ gem 'devise'
 
 gem "faker", "~> 2.19"
 gem 'dotenv-rails', groups: [:development, :test]
+gem 'cloudinary', '~> 1.16.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+require "open-uri"
+
 User.destroy_all
 Friend.destroy_all
 
@@ -24,23 +26,31 @@ hannah.save!
 10.times do
   friend = Friend.new(name: Faker::Name.name, age: rand(18..110), location: Faker::Address.city, gender: Faker::Gender.type, interest: Faker::Verb.ing_form, price: rand(0..10000))
   friend.user = User.first
+  file = URI.open('https://clasebcn.com/wp-content/uploads/2020/04/harold-thumb.jpg')
+  friend.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
   friend.save!
 end
 
 10.times do
   friend = Friend.new(name: Faker::Name.name, age: rand(18..110), location: Faker::Address.city, gender: Faker::Gender.type, interest: Faker::Verb.ing_form, price: rand(0..10000))
   friend.user = User.second
+  file = URI.open('https://i.guim.co.uk/img/media/3aab8a0699616ac94346c05f667b40844e46322f/0_123_5616_3432/master/5616.jpg?width=445&quality=45&auto=format&fit=max&dpr=2&s=cdfeb9fcf58f8dd0d5dcb70ec4fb6673')
+  friend.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
   friend.save!
 end
 
 10.times do
   friend = Friend.new(name: Faker::Name.name, age: rand(18..110), location: Faker::Address.city, gender: Faker::Gender.type, interest: Faker::Verb.ing_form, price: rand(0..10000))
   friend.user = User.third
+  file = URI.open('https://www.ladbible.com/cdn-cgi/image/width=720,quality=70,format=jpeg,fit=pad,dpr=1/https%3A%2F%2Fs3-images.ladbible.com%2Fs3%2Fcontent%2F733ff9e65fdbed5b62b45728a7a76145.png')
+  friend.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
   friend.save!
 end
 
 10.times do
   friend = Friend.new(name: Faker::Name.name, age: rand(18..110), location: Faker::Address.city, gender: Faker::Gender.type, interest: Faker::Verb.ing_form, price: rand(0..10000))
   friend.user = User.fourth
+  file = URI.open('https://hungarytoday.hu/wp-content/uploads/2020/06/Hide-the-Pain-Harold-prof..jpg')
+  friend.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
   friend.save!
 end
